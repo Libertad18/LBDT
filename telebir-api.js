39dc34c2003fb5f876eaeaebd48a10f297aeed60
Libertad18 <arnetaddisu7@gmail.com>
2025-07-30 07:26:44 -0700

    Create telebir-api.js

---
+++ telebir-api.js
@@ -0,0 +1,5 @@
+// telebirr-api.js (Mock version for demo)
+function sendToTelebirr(phone, amount) {
+  alert(`Send ${amount} birr to developer at 0943838862`);
+  // You can use fetch() to send this to a backend that uses the real Telebirr API
+}
